191016e19be403242f0e6acbe4f652af3ea2cc73
Boris Ershov <b.ershov@nixys.ru>
2017-08-22 23:54:17 +0700

    Added 'unix-sock' collection

--- src/nxs-chat-srv-collections.h
+++ src/nxs-chat-srv-collections.h
@@ -6,6 +6,7 @@
 /*%%_NXS_TPL_HEADERS_BEGIN_%%*/
 #include <collections/mysql/mysql.h>
 #include <collections/tlgrm/tlgrm.h>
+#include <collections/unix-sock/unix-sock.h>
 /*%%_NXS_TPL_HEADERS_END_%%*/
 
 #endif /* _INCLUDE_NXS_CHAT_SRV_COLLECTIONS_H */
