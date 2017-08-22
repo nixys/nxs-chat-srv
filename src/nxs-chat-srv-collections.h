3b24d6f2847c8eb20f5b969596bf1bc13d39a289
Boris Ershov <b.ershov@nixys.ru>
2017-08-23 04:22:26 +0700

    Added collection to work with internal queue communications

--- src/nxs-chat-srv-collections.h
+++ src/nxs-chat-srv-collections.h
@@ -7,6 +7,7 @@
 #include <collections/mysql/mysql.h>
 #include <collections/tlgrm/tlgrm.h>
 #include <collections/unix-sock/unix-sock.h>
+#include <collections/queue-com/queue-com.h>
 /*%%_NXS_TPL_HEADERS_END_%%*/
 
 #endif /* _INCLUDE_NXS_CHAT_SRV_COLLECTIONS_H */
--- src/nxs-chat-srv-meta.h
+++ src/nxs-chat-srv-meta.h
@@ -6,6 +6,7 @@
 /*%%_NXS_TPL_HEADERS_BEGIN_%%*/
 #include <meta/tlgrm.h>
 #include <meta/db-users.h>
+#include <meta/queue-com.h>
 /*%%_NXS_TPL_HEADERS_END_%%*/
 
 #endif /* _INCLUDE_NXS_CHAT_SRV_META_H */
